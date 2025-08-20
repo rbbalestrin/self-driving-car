31372ea7dc542b7964caebfe9df6fca311426be3
Renan Bez <therealrenanbez@gmail.com]>
2025-08-20 13:47:18 -0300

    feat: added friction and maxSpeed

---
+++ controls.js
@@ -0,0 +1,42 @@
+class Controls {
+  constructor() {
+    this.left = false;
+    this.right = false;
+    this.forward = false;
+    this.reverse = false;
+    this.#addKeyboardListener()
+  }
+
+  #addKeyboardListener() {
+    document.onkeydown = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.left = true
+          break
+        case "ArrowRight":
+          this.right = true
+          break
+        case "ArrowUp":
+          this.forward = true
+          break
+        case "ArrowDown":
+          this.reverse = true
+          break
+      }
+      console.table(this)
+    }
+    document.onkeyup = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.left = false
+        case "ArrowRight":
+          this.right = false
+        case "ArrowUp":
+          this.forward = false
+        case "ArrowDown":
+          this.reverse = false
+      }
+      console.table(this)
+    }
+  }
+}
